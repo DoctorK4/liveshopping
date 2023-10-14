bc1574b62592d20532e6cd81455bbb4709087570
doctork4 <doctor.k1994@gmail.com>
2023-10-15 05:43:53 +0900

    gnb 생성

--- shopping-mall/src/pages/_layout.tsx
+++ shopping-mall/src/pages/_layout.tsx
@@ -3,12 +3,14 @@ import { Outlet } from "react-router-dom";
 import { QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { getClient } from "../queryClient";
+import Gnb from "../components/gnb";
 
 const Layout: React.FC = () => {
   const queryClient = getClient();
 
   return (
     <QueryClientProvider client={queryClient} >
+      <Gnb />
       <Suspense fallback={"loading..."}>
         <Outlet />
       </Suspense>
